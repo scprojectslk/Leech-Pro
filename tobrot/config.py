85673062ff5dde1b753f4a258c30edcb89edd0fd
SC Projects LK <80474245+scprojectslk@users.noreply.github.com>
2021-08-05 14:12:21 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,27 @@
+# Read Readme before doing anything
+
+
+#________COMPULSORY____________
+
+TG_BOT_TOKEN= "1869011996:AAEp2VAky486XKBoHIUtqWFdfVaEnfOZoHg"
+APP_ID = "2969667"
+API_HASH = "b0a122842abdeb035ed7c41adeaae495"
+OWNER_ID = "1549449621"
+
+# Add different ID separated by a SPACE (Like :: AUTH_CHANNEL = -10012503747 -10012586941 539295917)
+AUTH_CHANNEL = 
+
+
+#__________NOT ________COMPULSORY____________ ::
+
+# For RCLONE ::
+# Name of the folder....not ID
+DESTINATION_FOLDER = "TorrentLeech-Gdrive"
+INDEX_LINK = ""
+
+# OTHERS ::
+EDIT_SLEEP_TIME_OUT = 15
+MAX_TG_SPLIT_FILE_SIZE = 1072864000
+
+#_____There are other vars too_____Read Readme to Know better____
+#_____And fill them as new line if needed :)
