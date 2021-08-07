e9e48ce989c06d157f23c89b2ebd0cb6ab4f464c
SC Projects LK <80474245+scprojectslk@users.noreply.github.com>
2021-08-07 16:46:37 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -77,7 +77,7 @@ class Progress:
             elapsed_time = TimeFormatter(milliseconds=elapsed_time)
             estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-            progress = "<b> ✦━━✦ Status : Uploading ✦━━✦: 〘{2}%〙 📤⌉</b>\n│ \n<b>├〖{0}{1}〗</b>\n".format(
+            progress = "<b> ✦━━✦ Status : Uploading ✦━━✦: Uploaded ➡️ {2}% </b>\n│ \n<b>├〖{0}{1}〗</b>\n".format(
                 ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
                 ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
